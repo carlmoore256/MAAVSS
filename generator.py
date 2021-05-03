f7db30c10d3b3776eb11a1573b3736eedaf5d4f0
unknown <carlmoore256@gmail.com>
2021-05-02 19:06:48 -0700

    additions to datagen

--- generator.py
+++ generator.py
@@ -9,6 +9,9 @@ class DataGenerator():
 
     # num vid frames -> how many video frames to extract flow data from, size of 4D tensor
     def __init__(self, batch_size, num_vid_frames=4, framerate=30, samplerate=16000, data_path="./data/raw"):
+
+        assert batch_size > 1
+
         self.batch_size = batch_size
         self.num_vid_frames = num_vid_frames
         self.framerate = framerate
@@ -49,47 +52,54 @@ class DataGenerator():
 
     # waveform audio -> FFT (tf.complex64 dtype)
     def fft(self, audio):
-      fft = tf.signal.fft(audio)
-      if self.normalize:
-        fft = self.normalize_fft(fft)
-      return fft
+        audio = tf.cast(audio, dtype=tf.complex64)
+        fft = tf.signal.fft(audio)
+        if self.normalize:
+            fft = self.normalize_fft(fft)
+        return fft
 
     # normalize fft by 1/(length/2) 
     def normalize_fft(self, fft):
-      scalar = 1.0/(fft.shape[0] // 2)
-      normalized_fft = tf.math.multiply(fft, scalar)
-      return normalized_fft
+        scalar = 1.0/(fft.shape[0] // 2)
+        normalized_fft = tf.math.multiply(fft, scalar)
+        return normalized_fft
 
     def reverse_normalize_fft(self, normalized_fft):
-      return normalized_fft * (normalized_fft.shape[0] * 2)
+        return normalized_fft * (normalized_fft.shape[0] * 2)
 
     # x + y(i) -> magnitude, angle
     def rectangular_to_polar(self, rectangular):
-      magnitude = tf.abs(rectangular)
-      angle = tf.math.angle(rectangular)
-      polar = tf.concat([magnitude, angle], axis=2)
-      return polar
+        magnitude = tf.abs(rectangular)
+        angle = tf.math.angle(rectangular)
+        polar = tf.concat([magnitude, angle], axis=2)
+        return polar
 
+    def polar_to_rectangular(self, polar):
+        real = polar[:, :, 0] * np.cos(polar[:, :, 1])
+        imag = polar[:, :, 1] * np.cos(polar[:, :, 1])
+        concat = tf.concat([real, imag], axis=-1)
+        cmplx = self.ri_to_complex(concat)
+        return cmplx
 
     # float32 tensor to rectangular notation:
     # [real, imaginary] -> [complex,]
-    def complex_to_ri(self, tensor):      
-      real = tf.math.real(tensor)
-      imag = tf.math.imag(tensor)
-      ri_t = tf.concat([real, imag], axis=2)
-      return ri_t
+    # def complex_to_ri(self, tensor):      
+    #     real = tf.math.real(tensor)
+    #     imag = tf.math.imag(tensor)
+    #     ri_t = tf.concat([real, imag], axis=2)
+    #     return ri_t
 
     # rectangular notation to float32 tensor
     # [complex,] -> [real, imaginary]
     def ri_to_complex(self, tensor):      
-      real = tensor[:,:,0]
-      imag = tensor[:,:,1]
-      # account for FFT mirror cutoff at N/2+1
-      mirror = tf.zeros_like(real)
-      real = tf.concat([real,mirror], axis=1)
-      imag = tf.concat([imag,mirror], axis=1)
-      complex_t = tf.dtypes.complex(real, imag)
-      return complex_t
+        real = tensor[:,:,0]
+        imag = tensor[:,:,1]
+        # account for FFT mirror cutoff at N/2+1
+        mirror = tf.zeros_like(real)
+        real = tf.concat([real,mirror], axis=1)
+        imag = tf.concat([imag,mirror], axis=1)
+        complex_t = tf.dtypes.complex(real, imag)
+        return complex_t
 
 
     # load audio and video exaple pair
@@ -108,7 +118,6 @@ class DataGenerator():
         print(f'AUDIO SHAPE {audio.shape}')
 
         return frames, audio
-
 
     def load_video(self, path):
         cap = cv2.VideoCapture(path)
@@ -129,9 +138,19 @@ class DataGenerator():
         waveform = tf.audio.decode_wav(raw_audio, desired_channels=1)
         return waveform[0]
 
+    # main process for generating x,y pairs
+    def gen_xy(self, frames, audio):
+        v_clip, a_clip = self.get_random_clip(frames, audio)
+        a_clip_noise = self.add_noise(a_clip)
+        # take fft of audio & convert from rectangular (x + y(i)) into polar (magnitude, phase)
+        # added noise
+        ft_polar_x = rectangular_to_polar(self.fft(a_clip_noise))
+        # ground truth
+        fft_polar_y = rectangular_to_polar(self.fft(a_clip))
+
+        return ft_polar_x, fft_polar_y, v_clip
 
     def generator(self):
-
         while True:
             frames, audio = self.load_example_pair(self.all_vids[self.example_idx])
 
@@ -140,18 +159,33 @@ class DataGenerator():
             if self.example_idx > len(self.all_vids):
                 self.example_idx = 0
 
-            x = []
-            y = []
+            x_ft, y_ft, v_clip = self.gen_xy(frames, audio)
+
+
+            x_ft = tf.expand_dims(x_ft, axis=0)
+            x_vid = tf.expand_dims(v_clip, axis=0)
+
+            y_ft = tf.expand_dims(y_ft, axis=0)
+            y_vid = tf.expand_dims(v_clip, axis=0)
 
             # creates batch on the same video
-            for i in range(self.batch_size):
-                v_clip, a_clip = self.get_random_clip(frames, audio)
+            for _ in range(self.batch_size - 1):
+                this_x_ft, this_y_ft, this_v_clip = self.gen_xy(frames, audio)
                 
+                x_ft = tf.concat([x_ft, this_x_ft], 0)
+                y_ft = tf.concat([y_ft, this_y_ft], 0)
 
+                x_vid = tf.concat([x_vid, this_v_clip], 0)
+                y_vid = tf.concat([y_vid, this_v_clip], 0)
 
+                # x.append([fft_x, v_clip])
+                # y.append([fft_y, v_clip])
 
-                       a_clip = tf.cast(a_clip, dtype=tf.complex64)
+            return [[x_ft, x_vid], [y_ft, y_vid]] 
 
-        fft_clip = tf.signal.fft(a_clip)
+            # x = np.asarray(x)
+            # y = np.asarray(y)
 
-dg = DataGenerator(32)
+if __name__ == "__main__":
+    print("remove me")
+    # dg = DataGenerator(32)
