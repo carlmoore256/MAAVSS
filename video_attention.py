49e459af50863dbb26bc7f4e0eed8790140937b7
unknown <carlmoore256@gmail.com>
2021-06-08 19:53:28 -0700

    cleaning up video attn

--- video_attention.py
+++ video_attention.py
@@ -4,141 +4,25 @@
 
 import enum
 import os
-import glob
-import sys
-import argparse
-import cv2
 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 import torchvision
 from torchvision import transforms as pth_transforms
-import numpy as np
-from PIL import Image
+# import numpy as np
 
 import dino.utils
 import dino.vision_transformer as vits
 
 
-FOURCC = {
-    "mp4": cv2.VideoWriter_fourcc(*"MP4V"),
-    "avi": cv2.VideoWriter_fourcc(*"XVID"),
-}
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
 class VideoAttention:
     def __init__(self, resize=None):
         self.resize = resize
-        # self.model = None
-        # Don't need to load model if you only want a video
-        # if not self.args.video_only:
-
         self.model = self.__load_model()
-
-    def run(self):
-        if self.args.input_path is None:
-            print(f"Provided input path {self.args.input_path} is non valid.")
-            sys.exit(1)
-        else:
-            if self.args.video_only:
-                self._generate_video_from_images(
-                    self.args.input_path, self.args.output_path
-                )
-            else:
-                # If input path exists
-                if os.path.exists(self.args.input_path):
-                    # If input is a video file
-                    if os.path.isfile(self.args.input_path):
-                        frames_folder = os.path.join(self.args.output_path, "frames")
-                        attention_folder = os.path.join(
-                            self.args.output_path, "attention"
-                        )
-
-                        os.makedirs(frames_folder, exist_ok=True)
-                        os.makedirs(attention_folder, exist_ok=True)
-
-                        self._extract_frames_from_video(
-                            self.args.input_path, frames_folder
-                        )
-
-                        self._inference(
-                            frames_folder,
-                            attention_folder,
-                        )
-
-                        self._generate_video_from_images(
-                            attention_folder, self.args.output_path
-                        )
-
-                    # If input is a folder of already extracted frames
-                    if os.path.isdir(self.args.input_path):
-                        attention_folder = os.path.join(
-                            self.args.output_path, "attention"
-                        )
-
-                        os.makedirs(attention_folder, exist_ok=True)
-
-                        self._inference(self.args.input_path, attention_folder)
-
-                        self._generate_video_from_images(
-                            attention_folder, self.args.output_path
-                        )
-
-                # If input path doesn't exists
-                else:
-                    print(f"Provided input path {self.args.input_path} doesn't exists.")
-                    sys.exit(1)
-
-    def _extract_frames_from_video(self, inp: str, out: str):
-        vidcap = cv2.VideoCapture(inp)
-        self.args.fps = vidcap.get(cv2.CAP_PROP_FPS)
-
-        print(f"Video: {inp} ({self.args.fps} fps)")
-        print(f"Extracting frames to {out}")
-
-        success, image = vidcap.read()
-        count = 0
-        while success:
-            cv2.imwrite(
-                os.path.join(out, f"frame-{count:04}.jpg"),
-                image,
-            )
-            success, image = vidcap.read()
-            count += 1
-
-    def _generate_video_from_images(self, inp: str, out: str):
-        img_array = []
-        attention_images_list = sorted(glob.glob(os.path.join(inp, "attn-*.jpg")))
-
-        # Get size of the first image
-        with open(attention_images_list[0], "rb") as f:
-            img = Image.open(f)
-            img = img.convert("RGB")
-            size = (img.width, img.height)
-            img_array.append(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
-
-        print(f"Generating video {size} to {out}")
-
-        for filename in tqdm(attention_images_list[1:]):
-            with open(filename, "rb") as f:
-                img = Image.open(f)
-                img = img.convert("RGB")
-                img_array.append(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
-
-        out = cv2.VideoWriter(
-            os.path.join(out, "video." + self.args.video_format),
-            FOURCC[self.args.video_format],
-            self.args.fps,
-            size,
-        )
-
-        for i in range(len(img_array)):
-            out.write(img_array[i])
-        out.release()
-        print("Done")
 
     def _inference(self, frames):
 
@@ -280,83 +164,6 @@ class VideoAttention:
                     "There is no reference weights available for this model => We use random weights."
                 )
         return model
-
-
-def parse_args():
-    parser = argparse.ArgumentParser("Generation self-attention video")
-    parser.add_argument(
-        "--arch",
-        default="vit_small",
-        type=str,
-        choices=["vit_tiny", "vit_small", "vit_base"],
-        help="Architecture (support only ViT atm).",
-    )
-    parser.add_argument(
-        "--patch_size", default=8, type=int, help="Patch resolution of the self.model."
-    )
-    parser.add_argument(
-        "--pretrained_weights",
-        default="",
-        type=str,
-        help="Path to pretrained weights to load.",
-    )
-    parser.add_argument(
-        "--checkpoint_key",
-        default="teacher",
-        type=str,
-        help='Key to use in the checkpoint (example: "teacher")',
-    )
-    parser.add_argument(
-        "--input_path",
-        required=True,
-        type=str,
-        help="""Path to a video file if you want to extract frames
-            or to a folder of images already extracted by yourself.
-            or to a folder of attention images.""",
-    )
-    parser.add_argument(
-        "--output_path",
-        default="./",
-        type=str,
-        help="""Path to store a folder of frames and / or a folder of attention images.
-            and / or a final video. Default to current directory.""",
-    )
-    parser.add_argument(
-        "--threshold",
-        type=float,
-        default=0.6,
-        help="""We visualize masks
-        obtained by thresholding the self-attention maps to keep xx percent of the mass.""",
-    )
-    parser.add_argument(
-        "--resize",
-        default=None,
-        type=int,
-        nargs="+",
-        help="""Apply a resize transformation to input image(s). Use if OOM error.
-        Usage (single or W H): --resize 512, --resize 720 1280""",
-    )
-    parser.add_argument(
-        "--video_only",
-        action="store_true",
-        help="""Use this flag if you only want to generate a video and not all attention images.
-            If used, --input_path must be set to the folder of attention images. Ex: ./attention/""",
-    )
-    parser.add_argument(
-        "--fps",
-        default=30.0,
-        type=float,
-        help="FPS of input / output video. Automatically set if you extract frames from a video.",
-    )
-    parser.add_argument(
-        "--video_format",
-        default="mp4",
-        type=str,
-        choices=["mp4", "avi"],
-        help="Format of generated video (mp4 or avi).",
-    )
-
-    return parser.parse_args()
 
 
 # if __name__ == "__main__":
